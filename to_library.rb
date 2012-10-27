4d2dab33e5db0f043b15870eddcc786db6d6c266
AlexS <alexandru.sirbu@hostway.ro>
2012-10-27 22:14:04 +0300

    improved to_json

--- to_library.rb
+++ to_library.rb
@@ -1,18 +1,14 @@
 class FDC
 	
 	def self.to_json s
+		s=stringify(eval(s)).to_s
 		s.gsub("=>",":")	
 	end
-
-	protected
 	
-	def self.transform_symbols_to_string s
-		is_string=false
-		for i in 0..s.length-1 
-			if (s[i]=="\"") 
-				is_string != is_string
-			end
-		end
+	def self.stringify(obj)
+    return obj.inject({}){|memo,(k,v)| memo[k.to_s] =  stringify(v); memo} if obj.is_a? Hash
+    return obj.inject([]){|memo,v    | memo           << stringify(v); memo} if obj.is_a? Array
+    return obj
 	end
 
 end
