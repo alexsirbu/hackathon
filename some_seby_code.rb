0911d85d80ec6d55d53b23af4113e76ce9745610
dankhus <dankhus@yahoo.com>
2012-10-28 11:17:08 +0200

    some_seby_code

--- some_seby_code.rb
+++ some_seby_code.rb
@@ -1,45 +1,48 @@
 class FDC
 
 	def self.to_xml s
+		kont = 1
 		n = 0
 		name_parent = []
 		sxml = ""
-		for i in 0..s.length-1
-			if (s[i..i+1] == "{\"") 
-				sxml << "<"
-				saux = s.clone
-				n = n+1
-				name_parent[n] = saux.slice(i+2..(saux.index('"', i+2)-1))
-				if (!name_parent[n].nil?) 
-					sxml << name_parent[n] << ">"
-				end
-			elsif (s[i] == "}")
+			for i in 0..s.length-1
+				if (s[i..i+1] == "{\"") 
+					sxml << "<"
+					saux = s.clone
+					n = n+1
+					name_parent[n] = saux.slice(i+2..(saux.index('"', i+2)-1))
+					if (!name_parent[n].nil?) 
+						sxml << name_parent[n] << ">"
+					end
+				elsif (s[i] == "}")
 					sxml << "</" 
-					print name_parent
 					sxml << name_parent[n]
 					n = n-1
 					sxml << ">"
-			elsif (s[i..i+1] == "=>")
-				if (s[i+2] == "[")
-				elsif saux = s.clone
-				sxml << saux.slice(i+2..(saux.index("}", i+2)-1))
+				elsif (s[i..i+1] == "=>")
+					if (s[i+2] == "[")
+					elsif saux = s.clone
+					sxml << saux.slice(i+2..(saux.index("}", i+2)-1))
+					end
 				end
 			end
-		end
 		sxml
 	end
 
 	def self.from_xml s
 		shash = ""
-		arbre = []
 		for i in 0..s.length-2
 			if (s[i] == ">") 
 				if (s[i+1] == "<")
-					if (s[i+2] != "/") then shash << '"=>['
+					if (s[i+2] != "/") 
+						saux = s.clone
+						if (s[saux.index(">", i+2)+2 .. saux.index(">", i+2)+3] == "</") then shash << '"=>['
+						else shash<< ","
+						end
 					end
 				else shash << '"=>'
 				saux = s.clone
-				shash << saux.slice(i+1..(saux.index("<", i+2)-1))
+				shash << '"' << saux.slice(i+1..(saux.index("<", i+2)-1)) << '"'
 				end
 			elsif (s[i] == "<")
 				if (s[i+1] == "/") 
